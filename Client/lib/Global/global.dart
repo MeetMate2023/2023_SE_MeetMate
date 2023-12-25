bb1a4080611808ff850a62f2b5b917611db3dd93
정명기 <jmk101711@naver.com>
2023-12-25 14:58:08 +0900

    ""

--- Client/lib/Global/global.dart
+++ Client/lib/Global/global.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 Dio dio =Dio(); // http통신을 위한 Dio객체 생성
-String baseUrl = "https://bf50-165-229-86-168.ngrok-free.app"; // 서버 baseUrl
+String baseUrl = "https://0302-165-229-86-168.ngrok-free.app"; // 서버 baseUrl
 
 class User{ // 유저 정보 저장
   late String User_Local ='';
